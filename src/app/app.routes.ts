71d857d90eb9350ff9854aedbf956c65c02b03f6
UrPapuh <andrewgr073@gmail.com>
2024-03-21 20:24:41 +0000

    basic version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ForDirectiveComponent } from './for-directive/for-directive.component'; 
+import { IfDirectiveComponent } from './if-directive/if-directive.component';
+import { SwitchDirectiveComponent } from './switch-directive/switch-directive.component';
+
+import { NgmodelAttributeComponent } from './ngmodel-attribute/ngmodel-attribute.component';
+import { NgclassAttributeComponent } from './ngclass-attribute/ngclass-attribute.component';
+import { NgstyleAttributeComponent } from './ngstyle-attribute/ngstyle-attribute.component';
+
+export const routes: Routes = [
+  { path: 'for-directive', component: ForDirectiveComponent },
+  { path: 'if-directive', component: IfDirectiveComponent },
+  { path: 'switch-directive', component: SwitchDirectiveComponent },
+  { path: 'ngmodel-attribute', component: NgmodelAttributeComponent },
+  { path: 'ngclass-attribute', component: NgclassAttributeComponent },
+  { path: 'ngstyle-attribute', component: NgstyleAttributeComponent },
+];
