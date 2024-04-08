be98694bb7c241eb4d41a6fe575079e1a5e9a864
angaro073 <andrewgarciarodriguez04@gmail.com>
2024-04-08 16:39:17 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <div>
+      <h1>User List</h1>
+      @for (user of users; track $index) {
+        <p>{{user.name}}</p>
+      }
+      @empty {
+        <p>Users not found!</p>
+      }
+    </div>
+  `,
+})
+export class AppComponent implements OnInit {
+  private readonly apiURL: string = "https://jsonplaceholder.typicode.com/users";
+  protected users: User[] = [];
+
+  constructor(protected httpClient: HttpClient) {}
+
+  getUsers() {
+    this.httpClient.get<User[]>(this.apiURL).subscribe(res => this.users = res);
+  }
+  ngOnInit() {
+    this.getUsers();
+  }
+}
